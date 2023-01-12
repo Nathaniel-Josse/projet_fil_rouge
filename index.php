64ced8ff771d3be7768d5f40faebe31e0ef311bd
Nathaniel <nathaniel.josse@gmail.com>
2023-01-12 18:03:02 +0100

    Ajout de script.js et du favicon

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name ="robots" content="all">    
     <title>Portfolio Web</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Importation de Bootstrap pour le carrousel --> <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="asset/css/style.min.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="asset/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="asset/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="asset/img/favicon-16x16.png">
+    <link rel="manifest" href="asset/img/site.webmanifest">
 </head>
 <body>
     <div class="containerStart">
@@ -13,9 +17,9 @@
             <img src="asset/img/logotype_nathaniel_test.png" alt="logo" class="logo">
             <nav>
                 <ul>
-                    <li><a href="#" title="Accueil">Accueil</a></li>
-                    <li><a href="#" title="Realisations">Réalisations</a></li>
-                    <li><a href="#" title="Contact">Contact</a></li>
+                    <li><a href="#" title="accueil">Accueil</a></li>
+                    <li><a href="#" title="realisations">Réalisations</a></li>
+                    <li><a href="#" title="contact">Contact</a></li>
                 </ul>
             </nav>
         </header>
